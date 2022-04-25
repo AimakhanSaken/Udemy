865589b7f6b6717e4952e5fb63beb125817d02ad
Saken <aimakhan@mail.ru>
2022-04-25 13:10:27 +0600

    home work

--- jsforkids.js
+++ jsforkids.js
@@ -113,7 +113,45 @@ var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
 // Выбор случайного прилагательного из массива randomAdjectives:
 var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
  // Выбор случайного слова из массива randomWords:
-var randomWords = randomWords[Math.floor(Math.random() * 5)];
+var randomWords = randomWords[Math.floor(Math.random() * randomWords.length)];
 // Соединяем случайные строки в предложение:
 var randomInsult = "У тебя " + randomBodyPart + " словно " + randomAdjective + " " + randomWord + "!!!";
 randomInsult;
+
+//----------------------------------HOME WORK---------------------------------------------------------
+var randomFamMembers = ["Batya", "vtoroy Batya", "Mama", "Detina"];
+var randomAdjec =  ["Ynilaya", "vonuchaya", "Edkaya"];
+var randomSlova = ["Zhirobasina", "Zhertva incesta", "Paskyda", "hyibaloEbannoe"];
+var randomFamMembers = randomFamMembers[Math.floor(Math.random() * randomFamMembers.length)];
+var randomAdjec = randomAdjec[Math.floor(Math.random() * randomAdjec.length)];
+var randomSlova = randomSlova[Math.floor(Math.random() * randomSlova.length)];
+var randomOskorbleniya = "Y tebya " + randomFamMembers + " pzdc " + randomAdjec + " blyat' " + randomSlova + " MyaahaHaahha";
+randomOskorbleniya;
+
+//------------------------------------------------------------------------------------------------
+var randomBodyPart = ["Golova", "Nogy", "Zhopa", "Ryki", "Glaza", "Rot"];
+var randomAdjectives = ["Bolshaya", "Volosataya", "Chernaya", "Vonuchaya", "Gnoynaya"];
+var randomAnimalBodyPart = ["Hvost", "Hobot", "Roga", "Gorb", "Kluv"];
+var randomAnimal = ["Krysy", "Vorony", "Slona", "Barana", "Zhaby"];
+var randomBodyPart = randomBodyPart[Math.floor(Math.random() * randomBodyPart.length)];
+var randomAdjectives = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)];
+var randomAnimalBodyPart = randomAnimalBodyPart[Math.floor(Math.random() * randomAnimalBodyPart.length)];
+var randomAnimal = randomAnimal[Math.floor(Math.random() * randomAnimal.length)];
+var randomInsult = "Y tebya " + randomBodyPart + " eshe bolee " + randomAdjectives + " chem " + randomAnimalBodyPart + " y " + randomAnimal + " Ahahhahahha, govno sobach'e ";
+randomInsult;
+
+//----------------------Join----------------------------------------------------------------------
+var randomBodyPart = ["Golova", "Nogy", "Zhopa", "Ryki", "Glaza", "Rot"];
+var randomAdjectives = ["Bolshaya", "Volosataya", "Chernaya", "Vonuchaya", "Gnoynaya"];
+var randomAnimalBodyPart = ["Hvost", "Hobot", "Roga", "Gorb", "Kluv"];
+var randomAnimal = ["Krysy", "Vorony", "Slona", "Barana", "Zhaby"];
+var randomBodyPart = randomBodyPart[Math.floor(Math.random() * randomBodyPart.length)];
+var randomAdjectives = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)];
+var randomAnimalBodyPart = randomAnimalBodyPart[Math.floor(Math.random() * randomAnimalBodyPart.length)];
+var randomAnimal = randomAnimal[Math.floor(Math.random() * randomAnimal.length)];
+var randomInsult = ["Y tebya ", randomBodyPart, " eshe bolee ", randomAdjectives, " chem ",  randomAnimalBodyPart," y ", randomAnimal, " Ahahhahahha, govno sobach'e "];
+randomInsult.join(" ");
+//-------------------------------------------------------------------------------------------------
+var int = [3, 2, 1];
+var str = [int[0], "bolshe, chem", int[1], "bolshe, chem", int[2]];
+str.join(" ");
