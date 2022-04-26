17a16ddc741bf6172d402c5b5d92de7dea6692db
Saken <aimakhan@mail.ru>
2022-04-26 17:43:05 +0600

    For

--- jsforkids.js
+++ jsforkids.js
@@ -216,3 +216,52 @@ while (sheepCounter < 10) {
         sheepCounter++;
 }
 console.log("ZZZzzzz");
+
+//-------------------------------------For----------------------------------------------
+var animals = ["лев", "фламинго", "белый медведь", "удав"];
+for (var i = 0; i < animals.length; i++) {
+    console.log("V etom zoo est " + animals[i] + ".");
+}
+
+for (x = 3; x < 10000; x = x * 3) {
+    console.log(x);
+}
+
+var x = 3;
+while (x < 10000) {
+    console.log(x);  
+    x = x * 3;
+}
+
+//------------------------------HomeWork-------------------------------------------------
+
+var animals = ["Cat", "Dog", "Horse"];
+for (i = 0; i < animals.length; i++) {
+    animals[i] = animals[i] + " is awesome";
+}
+console.log(animals);
+
+var randomString = "";
+var alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+while (randomString.length < 9) {
+    console.log(
+        (randomString += alphabet[Math.floor(Math.random() * alphabet.length)])
+    );
+}
+
+var input = "javascript is awesome";
+var output = "";
+for (i = 0; i < input.length; i++) {
+    if (input[i] === "a") {
+        output += "4";
+    }   else if (input[i] === "e") {
+        output += "3";
+    }   else if (input[i] === "o") {
+        output += "0";
+    }   else if (input[i] === "i") {
+        output += "1";
+    }   else {
+        output += input[i];
+    }
+}
+console.log(output);
